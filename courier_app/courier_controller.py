eb2049f4ef682365ea8bc4337b3581095babc335
Syed Haider Ali <haiderali@joyngroup.us>
2024-05-02 22:18:47 +0500

    2nd Changes

--- courier_app/courier_controller.py
+++ courier_app/courier_controller.py
@@ -11,104 +11,11 @@ from utils.helper import create_response, paginate_data
 from utils.response_messages import *
 # from vehicle.serializer import serializer
 
-from courier_site.settings import EMAIL_HOST_USER
+from chat_site.settings import EMAIL_HOST_USER
 from django.core.mail import send_mail
 from datetime import date, timedelta
 
 
-
-class ProductController:
-    serializer_class = ProductSerializer
-    filterset_class = ProductFilter
-
- 
-    def create(self, request):
-        try:
-            request.POST._mutable = True
-            request.data["created_by"] = request.user.guid
-            request.POST._mutable = False
-
-            if request.user.role in ['admin', 'manager'] or request.user.is_superuser:  # roles
-                validated_data = ProductSerializer(data=request.data)
-                if validated_data.is_valid():
-                    response = validated_data.save()
-                    response_data = ProductSerializer(response).data
-                    return Response({'data': response_data}, 200)
-                else:
-                    error_message = get_first_error_message(validated_data.errors, "UNSUCCESSFUL")
-                    return Response({'data': error_message}, 400)
-            else:
-                return Response({'data': "Permission Denaied"}, 400)
-        except Exception as e:
-            return Response({'error': str(e)}, 500)
-
-    # mydata = Member.objects.filter(firstname__endswith='s').values()
-    def get_product(self, request):
-        try:
-
-            instances = self.serializer_class.Meta.model.objects.all()
-
-            filtered_data = self.filterset_class(request.GET, queryset=instances)
-            data = filtered_data.qs
-
-            paginated_data, count = paginate_data(data, request)
-
-            serialized_data = self.serializer_class(paginated_data, many=True).data
-            response_data = {
-                "count": count,
-                "data": serialized_data,
-            }
-            return create_response(response_data, "SUCCESSFUL", 200)
-
-
-        except Exception as e:
-            return Response({'error': str(e)}, 500)
-
-    def update_product(self, request):
-        try:
-            if "id" in request.data:
-                # finding instance
-                instance = Product.objects.filter(id=request.data["id"]).first()
-
-                if instance:
-                    request.POST._mutable = True
-                    request.data["updated_by"] = request.user.guid
-                    request.POST._mutable = False
-
-                    # updating the instance/record
-                    serialized_data = ProductSerializer(instance, data=request.data, partial=True)
-                    if request.user.role in ['admin', 'manager'] or request.user.is_superuser:  # roles
-                        if serialized_data.is_valid():
-                            response = serialized_data.save()
-                            response_data = ProductSerializer(response).data
-                            return Response({"data": response_data}, 200)
-                        else:
-                            error_message = get_first_error_message(serialized_data.errors, "UNSUCCESSFUL")
-                            return Response({'data': error_message}, 400)
-                    else:
-                        return Response({'data': "Permission Denaied"}, 400)
-                else:
-                    return Response({"data": "NOT FOUND"}, 404)
-            else:
-                return Response({"data": "ID NOT PROVIDED"}, 400)
-
-        except Exception as e:
-            return Response({'error': str(e)}, 500)
-
-    def delete_product(self, request):
-        try:
-            if "id" in request.query_params:
-                instance = Product.objects.filter(id=request.query_params['id']).first()
-
-                if instance:
-                    instance.delete()
-                    return Response({"data": "SUCESSFULL"}, 200)
-                else:
-                    return Response({"data": "RECORD NOT FOUND"}, 404)
-            else:
-                return Response({"data": "ID NOT PROVIDED"}, 400)
-        except Exception as e:
-            return Response({'error': str(e)}, 500)
 
 
 class OrderController:
--- courier_app/courier_serializer.py
+++ courier_app/courier_serializer.py
@@ -3,16 +3,16 @@ from .models import Product, Order, OrderDetail
 from rest_framework.serializers import ModelSerializer
 from user_auth.user_serializer import UserListingSerializer
 
-class ProductSerializer(ModelSerializer):
-    class Meta:
-        model = Product
-        fields='__all__'
+# class ProductSerializer(ModelSerializer):
+#     class Meta:
+#         model = Product
+#         fields='__all__'
 
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        data['created_by'] = UserListingSerializer(instance.created_by).data if instance.created_by else None
-        data['updated_by'] = UserListingSerializer(instance.updated_by).data if instance.updated_by else None
-        return data
+#     def to_representation(self, instance):
+#         data = super().to_representation(instance)
+#         data['created_by'] = UserListingSerializer(instance.created_by).data if instance.created_by else None
+#         data['updated_by'] = UserListingSerializer(instance.updated_by).data if instance.updated_by else None
+#         return data
 
 
 class OrderSerializer(serializers.ModelSerializer):
